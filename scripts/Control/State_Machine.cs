61e86d7eb3316ab0772d3557d93bd7311f4b8b0d
Simon Moorleghen-Volpe <wscvolpe@gmail.com>
2023-09-18 15:50:43 -0400

    walk and idle state basis

--- scripts/Control/State_Machine.cs
+++ scripts/Control/State_Machine.cs
@@ -1,17 +1,19 @@
 using System;
 using Godot;
 
+
 public partial class State_Machine : Node {
     public override void _Ready(){
         SetMeta("brain", true);
-        // PlayerStates = new PlayerState[Enum.GetNames(typeof(Player_States)).Length];
+        PlayerStates = new PlayerState[Enum.GetNames(typeof(Player_States)).Length];
         Godot.Collections.Array<Node> childArray = GetChildren();
         foreach(Node child in childArray){
             if(child.HasMeta("PlayerState")){
-                
+                PlayerStates[(int)((PlayerState)child).State_ID] = (PlayerState)child;
             }
         }
     }
 
-    // private Player_States[]
+    private PlayerState[] PlayerStates;
+
 }
--- scripts/entities/player/PlayerState.cs
+++ scripts/entities/player/PlayerState.cs
@@ -3,12 +3,15 @@ using System;
 
 public partial class PlayerState : Node
 {
-	[export]
+	[Export]
 	public Player_States State_ID = Player_States.none;
 	public override void _Ready()
 	{
 		SetMeta("PlayerState", true);
 	}
 
+	public Player_States Operate(double delta){
+		return Player_States.none;
+	}
 
 }

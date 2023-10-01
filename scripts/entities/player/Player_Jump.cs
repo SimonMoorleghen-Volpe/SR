39bccff2015f84e58ab5b636763978d4a33ac244
Simon Moorleghen-Volpe <wscvolpe@gmail.com>
2023-09-30 22:41:48 -0400

    start developing jump state

--- scripts/entities/player/Player_Jump.cs
+++ scripts/entities/player/Player_Jump.cs
@@ -3,5 +3,21 @@ using System;
 
 public partial class Player_Jump : PlayerState
 {
-    
+
+    public override void _Ready()
+    {
+        base._Ready();
+        JumpVector = new(){X=0, Y=JumpHeight/JumpTime};
+        JumpTimer = new(){
+            OneShot=true,
+            WaitTime=JumpTime
+        };
+    }
+
+    [Export]
+    private float JumpTime = 0.5f;
+    [Export]
+    private float JumpHeight = 500;
+    private Vector2 JumpVector;
+    private Timer JumpTimer;
 }

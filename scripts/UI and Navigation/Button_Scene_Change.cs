9cc69bd3df5bb5a461dd3958cc6f9fd010cecfa7
Simon Moorleghen-Volpe <wscvolpe@gmail.com>
2023-10-09 23:23:36 -0400

    Change scene button created

---
+++ scripts/UI and Navigation/Button_Scene_Change.cs
@@ -0,0 +1,24 @@
+using Godot;
+using System;
+
+public partial class Button_Scene_Change : Button
+{
+    public override void _Ready()
+    {
+		Pressed += ChangeScene;
+    }
+
+	private void ChangeScene(){
+		foreach(Node child in GetTree().Root.GetChildren()){
+			if(child.HasMeta("scenemanager")){
+				((scenemanager)child).ChangeScene(TargetScene);
+				return;
+			}
+		}
+	}
+
+	[Export(PropertyHint.File)]
+	public string TargetScene = null;
+
+
+}
--- scripts/UI and Navigation/scenemanager.cs
+++ scripts/UI and Navigation/scenemanager.cs
@@ -8,4 +8,8 @@ public partial class scenemanager : Node
 	{
 	}
 
+	public void ChangeScene(string TargetScene){
+
+	}
+
 }

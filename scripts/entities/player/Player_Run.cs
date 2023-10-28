77f2240f3a1ff9e121559bba422e84edab85d781
Simon Moorleghen-Volpe <wscvolpe@gmail.com>
2023-10-28 18:06:36 -0400

    Improved walk and run reverse directions

--- scripts/entities/player/Player_Run.cs
+++ scripts/entities/player/Player_Run.cs
@@ -29,15 +29,17 @@ public partial class Player_Run : PlayerState
 			if(sum >= RunSpeedMax){
 				Change = (sum - RunSpeedMax) * Vector2.Right * (float)delta;
 			}
-		} else {
-			Change += Body.Velocity.Normalized() * IdleDeceleration * (float)delta * -1;
+		} 
+
+		if(Math.Sign(Change.X) != Math.Sign(Body.Velocity.X) | Change.X == 0){
+			Change.X += -Math.Sign(Body.Velocity.X) * IdleDeceleration * (float)delta;
 			if(Math.Abs(Change.X) > Math.Abs(Body.Velocity.X)){
 				Change.X = -Body.Velocity.X;
 				Body.Velocity += Change;
 				return "idle";
 			}
-			
-		}
+		}	
+		
 		Body.Velocity += Change;
         if(!Input.IsActionPressed("run")){
             return "walk";
@@ -46,6 +48,25 @@ public partial class Player_Run : PlayerState
 		return null;
     }
 
+    public override void Enter()
+    {
+		float d = 0.02f;
+		Vector2 Change = Vector2.Zero;
+        if(Input.IsActionPressed("move_left")){
+			Change += Vector2.Left * RunAcceleration * d;
+			float sum = MathF.Abs((Body.Velocity + Change).X);
+			if(sum >= RunSpeedMax){
+				Change = (sum - RunSpeedMax) * Vector2.Left * d;
+			}
+		} else if(Input.IsActionPressed("move_right")){
+			Change += Vector2.Right * RunAcceleration * d;
+			float sum = (Body.Velocity + Change).X;
+			if(sum >= RunSpeedMax){
+				Change = (sum - RunSpeedMax) * Vector2.Right * d;
+			}
+		}
+		Body.Velocity += Change;
+    }
     [Export]
     private float RunAcceleration = 200;
     [Export]
--- scripts/entities/player/Player_Walk.cs
+++ scripts/entities/player/Player_Walk.cs
@@ -9,7 +9,24 @@ public partial class Player_Walk : PlayerState
 		base._Ready();
 	}
 
-    public override void Enter(){	}
+    public override void Enter(){
+		float d = 0.02f;
+		Vector2 Change = Vector2.Zero;
+        if(Input.IsActionPressed("move_left")){
+			Change += Vector2.Left * WalkAcceleration * d;
+			float sum = MathF.Abs((Body.Velocity + Change).X);
+			if(sum >= WalkSpeedMax){
+				Change = (sum - WalkSpeedMax) * Vector2.Left * d;
+			}
+		} else if(Input.IsActionPressed("move_right")){
+			Change += Vector2.Right * WalkAcceleration * d;
+			float sum = (Body.Velocity + Change).X;
+			if(sum >= WalkSpeedMax){
+				Change = (sum - WalkSpeedMax) * Vector2.Right * d;
+			}
+		}
+		Body.Velocity += Change;
+	}
 
     public override string Take_Input(InputEvent @event)
     {
@@ -43,15 +60,17 @@ public partial class Player_Walk : PlayerState
 					return "run";
 				}
 			}
-		} else {
-			Change += Body.Velocity.Normalized() * Vector2.Left * IdleDeceleration * (float)delta;
+		}
+
+		if(Math.Sign(Change.X) != Math.Sign(Body.Velocity.X) | Change.X == 0){
+			Change.X += -Math.Sign(Body.Velocity.X) * IdleDeceleration * (float)delta;
 			if(Math.Abs(Change.X) > Math.Abs(Body.Velocity.X)){
 				Change.X = -Body.Velocity.X;
 				Body.Velocity += Change;
 				return "idle";
 			}
-			
 		}
+
 		Body.Velocity += Change;
 		
 		return null;
